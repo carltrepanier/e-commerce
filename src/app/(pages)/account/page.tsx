1d3d78365d831f2c66f72c44e4590af1ca846c4c
At0micTurtle <carltrep@videotron.ca>
2024-01-15 14:57:45 -0500

    Delete unused account-related files

--- src/app/(pages)/account/page.tsx
+++ src/app/(pages)/account/page.tsx
@@ -75,7 +75,7 @@ export default async function Account() {
                       <p>{purchase}</p>
                     ) : (
                       <h4>
-                        <Link href={`/products/${purchase.slug}`}>{purchase.title}</Link>
+                        {/* <Link href={`/products/${purchase.slug}`}>{purchase.title}</Link> */}
                       </h4>
                     )}
                   </li>
